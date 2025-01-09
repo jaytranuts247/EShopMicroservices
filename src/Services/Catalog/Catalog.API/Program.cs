e80b71b1c773b77e7dbf784015f8f8fb70ef3e40
jtran <tranquang2407@gmail.com>
2025-01-09 18:42:09 -0500

    remove old ErrorExceptionHandler

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -24,29 +24,6 @@ var app = builder.Build();
 
 app.MapCarter();
 
-// app.UseExceptionHandler(exceptionHandlerApp =>
-// {
-//     exceptionHandlerApp.Run(async context =>
-//     {
-//         var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-//         if (exception is null) return;
-//
-//         var problemDetails = new ProblemDetails
-//         {
-//             Title = exception.Message,
-//             Status = StatusCodes.Status500InternalServerError,
-//             Detail = exception.StackTrace
-//         };
-//
-//         var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-//         logger.LogError(exception, exception.Message);
-//
-//         context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-//         context.Response.ContentType = "application/problem+json";
-//
-//         await context.Response.WriteAsJsonAsync(problemDetails);
-//     });
-// });
 // app.MapGet("/", () => "Hello World!");
 
 app.Run();
